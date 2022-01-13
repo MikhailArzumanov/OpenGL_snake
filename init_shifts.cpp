275b802b453e404024c34d45b3e38039e277395a
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-13 10:48:30 +0300

    shifts were refactored

--- init_shifts.cpp
+++ init_shifts.cpp
@@ -1,11 +1,16 @@
 #include "functions.hpp"
 #include "snake.hpp"
 
+const point RIGHT_SHIFT = { 1,0 };
+const point UP_SHIFT	= { 0,1 };
+const point LEFT_SHIFT	= { -1,0 };
+const point DOWN_SHIFT	= { 0,-1 };
+
 void init_shifts() {
 	shifts = new point[DirsCount]{
-		{1,0}, //right
-		{0,1}, //up
-		{-1,0},//left
-		{0,-1} //down
+		RIGHT_SHIFT,
+		UP_SHIFT,
+		LEFT_SHIFT,
+		DOWN_SHIFT 
 	};
 }
