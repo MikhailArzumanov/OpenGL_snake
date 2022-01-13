4ab940b35c9f51e5db7a0b2aac8262c12a487861
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-13 10:40:46 +0300

    obj color initialization was refactored

--- init_obj_colors.cpp
+++ init_obj_colors.cpp
@@ -2,11 +2,16 @@
 #include "obj_colors.hpp"
 #include "game_objects.hpp"
 
+const Color WHITE = Color();
+const Color BLUE  = Color(0, 0, 255);
+const Color GREEN = Color(0, 255, 0);
+const Color RED   = Color(255, 0, 0);
+
 void init_obj_colors() {
     obj_colors = new Color[ObjCount];
-    obj_colors[emptiness] = Color();
-
-    obj_colors[apple] = Color(0, 0, 255);
-    obj_colors[snake] = Color(0, 255, 0);
-    obj_colors[wall] = Color(255, 0, 0);
+    
+    obj_colors[emptiness] = WHITE;
+    obj_colors[apple] = BLUE;
+    obj_colors[snake] = GREEN;
+    obj_colors[wall]  = RED;
 }
