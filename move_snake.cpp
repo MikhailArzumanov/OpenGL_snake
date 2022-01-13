b6efaf4945d04de75ad2eb6eaa2bb6dae0209d4c
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-13 11:06:17 +0300

    move_snake.cpp was refactored

--- move_snake.cpp
+++ move_snake.cpp
@@ -8,22 +8,35 @@ int mod(int x, int m) {
     return x % m;
 }
 
-void move_snake() {
-    point tail = snake_body.front();
-    point head = snake_body.back();
+void handle_apple(point tail) {
     if (!was_apple_eaten) {
-        table[tail.y][tail.x] = emptiness;
+        tail >> table = emptiness;
         snake_body.pop();
     }
     else spawn_apple();
+}
 
+void move_snake_head(point& head) {
     head = head + shifts[cur_direction];
     head.x = mod(head.x, table_width);
     head.y = mod(head.y, table_height);
+}
+
+void apply_snake_head_to_env(point head) {
+    head >> table = snake;
+    snake_body.push(head);
+}
+
+void move_snake() {
+    point tail = snake_body.front();
+    point head = snake_body.back();
+    
+    handle_apple(tail);
+
+    move_snake_head(head);
     check_head(head);
 
-    table[head.y][head.x] = snake;
-    snake_body.push(head);
+    apply_snake_head_to_env(head);
 
     last_direction = cur_direction;
 }
