ab870bc0c92611ccdaf50f4b147b814b913f013b
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-13 11:15:08 +0300

    window's name was changed

--- init_glut_window.cpp
+++ init_glut_window.cpp
@@ -4,7 +4,7 @@
 void init_glut_window() {
     glutInitWindowSize(900, 500);
     glutInitWindowPosition(10, 10);
-    glutCreateWindow("User_Name");
+    glutCreateWindow("OpenGL Snake");
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
